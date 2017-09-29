b5220539ced641098d31d64a31aba6e8261501a1
Vladimir Iglovikov <iglovikov@gmail.com>
2017-09-29 15:51:10 -0700

    Added more anchor sizes

--- src/rcnn/config.py
+++ src/rcnn/config.py
@@ -32,7 +32,7 @@ config.FIXED_PARAMS_SHARED = ['conv1', 'conv2', 'conv3', 'conv4', 'conv5']
 config.NUM_CLASSES = 2
 # config.SCALES = [(600, 1000)]  # first is scale (the shorter side); second is max size
 config.SCALES = [(720, 1280)]  # first is scale (the shorter side); second is max size
-config.ANCHOR_SCALES = (8, 16, 32)
+config.ANCHOR_SCALES = (4, 8, 16, 32, 64)
 config.ANCHOR_RATIOS = (0.5, 1, 2)
 config.NUM_ANCHORS = len(config.ANCHOR_SCALES) * len(config.ANCHOR_RATIOS)
 
